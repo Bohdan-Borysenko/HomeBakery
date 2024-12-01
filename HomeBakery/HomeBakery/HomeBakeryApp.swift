3c6b2a134616ddeabe1d4b6db35b7baec73d6736
Bohdan <borysenkobohdan@gmail.com>
2024-12-01 22:35:23 +0200

    none

---
+++ HomeBakery/HomeBakery/HomeBakeryApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeBakeryApp.swift
+//  HomeBakery
+//
+//  Created by Bohdan Borysenko on 01.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct HomeBakeryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
